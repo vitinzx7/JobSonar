55b464641a656a8bc354395050bcab642f01bd6f
vitinzx7 <victordideus@gmail.com>
2026-06-22 20:04:29 -0300

    cria os atributos da Vaga e seus getters e setters

---
+++ src/main/java/com/project/JobRadar/Vaga.java
@@ -0,0 +1,50 @@
+package com.project.JobRadar;
+
+public class Vaga {
+    private Long id;
+    private String name;
+    private String description;
+    private String city;
+    private String jobUrl;
+    private String publishedDate;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getJobUrl() {
+        return jobUrl;
+    }
+    public void setJobUrl(String jobUrl) {
+        this.jobUrl = jobUrl;
+    }
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    
+}
